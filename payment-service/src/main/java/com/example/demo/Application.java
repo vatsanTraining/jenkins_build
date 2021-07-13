ca1469c8e326f8395451b7f2a90020916c007e70
Admin <Admin@DESKTOP-F8V312A>
2021-07-13 07:55:48 +0530

    One More Example Added

---
+++ payment-service/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" OUT PUT FROM PAYMENT SERVICE - DONE");
+	}
+
+}
